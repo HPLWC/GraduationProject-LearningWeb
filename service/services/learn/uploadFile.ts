c5e971b0e4836c53b6a62ed124fc95e1c5a8e00b
hplwc <zhanghuipeng@moyi365.com>
2020-11-26 15:28:18 +0800

    fix: 上传接口调整

--- service/services/learn/uploadFile.ts
+++ service/services/learn/uploadFile.ts
@@ -11,32 +11,19 @@ let storage = (url) => {
   })
 }
 
-/*const storage = multer.diskStorage({
-  destination: 'static/upload/img/' + new Date().getFullYear() + ( new Date().getMonth() + 1 ) + new Date().getDate(),
-  filename (ctx, file, cb) {
-    const filenameArr = file.originalname.split('.')
-    cb(null,Date.now() + '.' + filenameArr[filenameArr.length - 1])
-  }
-})*/
-const upload = multer({storage: storage('img/')})
-async function imageUpload (ctx) {
-  const filename = ctx.req.file.filename
-  const desArr = ctx.req.file.destination.split('/')
-  desArr.shift()
-  const destination = desArr.join('/') + '/'
-  ctx.body = createBody({"uploaded":1, "fileName": filename, "url": destination, "default": destination + filename})
+const upload = (path) => {
+  return multer({storage: storage(path)})
 }
 
-const uploadSection = multer({storage: storage('section/')})
-async function sectionUpload (ctx) {
+async function uploadCtx (ctx) {
   const filename = ctx.req.file.filename
   const desArr = ctx.req.file.destination.split('/')
   desArr.shift()
   const destination = desArr.join('/') + '/'
-  ctx.body = createBody({"uploaded":1, "fileName": filename, "url": destination, "default": destination + filename})
+  ctx.body = createBody({'uploaded': 1, 'fileName': filename, 'url': destination, 'default': destination + filename})
 }
 
 export default (routes, prefix) => {
-  routes.post(prefix + '/image/upload', upload.single('upload'), imageUpload)
-  routes.post(prefix + '/section/upload', uploadSection.single('upload'), sectionUpload)
+  routes.post(prefix + '/image/upload', upload('img/').single('upload'), uploadCtx)
+  routes.post(prefix + '/section/upload', upload('section').single('upload'), uploadCtx)
 }
