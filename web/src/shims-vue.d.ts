91f3a30e4d4bf6fcb71bfe513003af049f58c416
hplwc <zhanghuipeng@moyi365.com>
2020-11-30 14:54:53 +0800

    fix: 添加全局声明

--- web/src/shims-vue.d.ts
+++ web/src/shims-vue.d.ts
@@ -1,4 +1,13 @@
+import Vue from 'vue'
+
 declare module '*.vue' {
-  import Vue from 'vue'
   export default Vue
+}
+
+declare module 'vue/types/vue' {
+  export interface Vue {
+    $c: any,
+    $ls: any,
+    $utils: any
+  }
 }
